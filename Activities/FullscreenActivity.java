8e515cc57bb69209fbd545cf7ffc506f251ea31d
learnernayak <44546059+learnernayak@users.noreply.github.com>
2018-10-28 20:21:24 +0530

    update full-screen activity

--- Activities/FullscreenActivity.java
+++ Activities/FullscreenActivity.java
@@ -10,7 +10,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.content.Intent;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Context;
